3f3c63a2737d0784bf92a9c9f5503660e004b82a
Francisco Perez-Sorrosal <fperez@yahoo-inc.com>
2012-11-28 19:04:58 +0100

    Use of 4 spaces instead of tabs. Author removed

--- src/test/java/com/yahoo/omid/TestUtils.java
+++ src/test/java/com/yahoo/omid/TestUtils.java
@@ -27,51 +27,49 @@ import org.apache.commons.logging.LogFactory;
  * 
  * This class contains functionality that is useful for the Omid tests.
  * 
- * @author Francisco Perez-Sorrosal - fperez@yahoo-inc.com
- *
  */
 public class TestUtils {
-	private static final Log LOG = LogFactory.getLog(TestUtils.class);
+    private static final Log LOG = LogFactory.getLog(TestUtils.class);
 
-	public static void waitForSocketListening(String host, int port,
-			int sleepTimeMillis) throws UnknownHostException, IOException,
-			InterruptedException {
-		while (true) {
-			Socket sock = null;
-			try {
-				sock = new Socket(host, port);
-			} catch (IOException e) {
-				// ignore as this is expected
-				Thread.sleep(sleepTimeMillis);
-				continue;
-			} finally {
-				if (sock != null) {
-					sock.close();
-				}
-			}
-			LOG.info("Host " + host + ":" + port + " is up");
-			break;
-		}
-	}
+    public static void waitForSocketListening(String host, int port,
+            int sleepTimeMillis) throws UnknownHostException, IOException,
+            InterruptedException {
+        while (true) {
+            Socket sock = null;
+            try {
+                sock = new Socket(host, port);
+            } catch (IOException e) {
+                // ignore as this is expected
+                Thread.sleep(sleepTimeMillis);
+                continue;
+            } finally {
+                if (sock != null) {
+                    sock.close();
+                }
+            }
+            LOG.info("Host " + host + ":" + port + " is up");
+            break;
+        }
+    }
 
-	public static void waitForSocketNotListening(String host, int port,
-			int sleepTimeMillis) throws UnknownHostException, IOException,
-			InterruptedException {
-		while (true) {
-			Socket sock = null;
-			try {
-				sock = new Socket(host, port);
-			} catch (IOException e) {
-				// ignore as this is expected
-				break;
-			} finally {
-				if (sock != null) {
-					sock.close();
-				}
-			}
-			Thread.sleep(sleepTimeMillis);
-			LOG.info("Host " + host + ":" + port + " is up");
-		}
-	}
+    public static void waitForSocketNotListening(String host, int port,
+            int sleepTimeMillis) throws UnknownHostException, IOException,
+            InterruptedException {
+        while (true) {
+            Socket sock = null;
+            try {
+                sock = new Socket(host, port);
+            } catch (IOException e) {
+                // ignore as this is expected
+                break;
+            } finally {
+                if (sock != null) {
+                    sock.close();
+                }
+            }
+            Thread.sleep(sleepTimeMillis);
+            LOG.info("Host " + host + ":" + port + " is up");
+        }
+    }
 
 }
