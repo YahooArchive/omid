YahooArchive/omid

commit ad4e46480ca99894b40dda7fddd70dab84f3745a

HBASE_META_CF constant redefined to byte[] to avoid useless conversions from String

src/main/java/com/yahoo/omid/notifications/TransactionCommittedRegionCoprocessor.java
@@ -73,8 +73,8 @@ public class TransactionCommittedRegionCoprocessor extends BaseRegionObserver {
                 if (!cf.equals(HBASE_META_CF)) {
                     String q = Bytes.toString(kv.getQualifier());
                     // Pattern for notify qualifier in framework's metadata column family: <cf>/<c>-notify
-                    put.add(Bytes.toBytes(HBASE_META_CF), Bytes.toBytes(cf + "/" + q + HBASE_NOTIFY_SUFFIX),
-                            kv.getTimestamp(), Bytes.toBytes("true"));
+                    put.add(HBASE_META_CF, Bytes.toBytes(cf + "/" + q + HBASE_NOTIFY_SUFFIX), kv.getTimestamp(),
+                            Bytes.toBytes("true"));
                 }
             }
         }
@@ -107,8 +107,8 @@ public class TransactionCommittedRegionCoprocessor extends BaseRegionObserver {
                 if (!cf.equals(HBASE_META_CF)) {
                     String q = Bytes.toString(kv.getQualifier());
                     // Pattern for notify qualifier in framework's metadata column family: <cf>/<c>-notify
-                    delete.deleteColumn(Bytes.toBytes(HBASE_META_CF),
-                            Bytes.toBytes(cf + "/" + q + HBASE_NOTIFY_SUFFIX), kv.getTimestamp());
+                    delete.deleteColumn(HBASE_META_CF, Bytes.toBytes(cf + "/" + q + HBASE_NOTIFY_SUFFIX),
+                            kv.getTimestamp());
                 }
             }
         }

src/main/java/com/yahoo/omid/notifications/Constants.java
@@ -15,9 +15,11 @@
  */
 package com.yahoo.omid.notifications;
 
+import org.apache.hadoop.hbase.util.Bytes;
+
 public class Constants {
-    
-    public static final String HBASE_META_CF = "onf-meta";
+
+    public static final byte[] HBASE_META_CF = Bytes.toBytes("onf-meta");
     public static final String HBASE_NOTIFY_SUFFIX = "-notify";
-    
+
 }

src/main/java/com/yahoo/omid/notifications/ScannerSandbox.java
@@ -159,7 +159,7 @@ public class ScannerSandbox {
             try { // TODO: This code should not be here in a production system
                   // because it disables the table to add a CF
                 HTableDescriptor tableDesc = admin.getTableDescriptor(this.interest.getTableAsHBaseByteArray());
-                if (!tableDesc.hasFamily(Bytes.toBytes(Constants.HBASE_META_CF))) {
+                if (!tableDesc.hasFamily(Constants.HBASE_META_CF)) {
                     String tableName = this.interest.getTable();
 
                     if (admin.isTableEnabled(tableName)) {
@@ -300,7 +300,7 @@ public class ScannerSandbox {
             }
 
             private void configureBasicScanProperties() {
-                byte[] cf = Bytes.toBytes(Constants.HBASE_META_CF);
+                byte[] cf = Constants.HBASE_META_CF;
                 // Pattern for observer column in framework's metadata column
                 // family: <cf>/<c>-notify
                 String column = interest.getColumnFamily() + "/" + interest.getColumn() + Constants.HBASE_NOTIFY_SUFFIX;

src/main/java/com/yahoo/omid/notifications/client/ObserverWrapper.java
@@ -31,13 +31,11 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.net.HostAndPort;
 import com.yahoo.omid.notifications.Constants;
-import com.yahoo.omid.notifications.NotificationException;
 import com.yahoo.omid.notifications.metrics.ClientSideAppMetrics;
 import com.yahoo.omid.notifications.thrift.generated.Notification;
 import com.yahoo.omid.transaction.RollbackException;
 import com.yahoo.omid.transaction.TTable;
 import com.yahoo.omid.transaction.Transaction;
-import com.yahoo.omid.transaction.TransactionException;
 import com.yahoo.omid.transaction.TransactionManager;
 import com.yammer.metrics.core.TimerContext;
 
@@ -110,9 +108,8 @@ public class ObserverWrapper implements Runnable {
                 continue;
             }
             TimerContext timer = metrics.startObserverInvocation(observer.getName());
-            notify(observer.getInterest().getTableAsHBaseByteArray(), notification.getRowKey(), observer
-                    .getInterest().getColumnFamilyAsHBaseByteArray(), observer.getInterest()
-                    .getColumnAsHBaseByteArray());
+            notify(observer.getInterest().getTableAsHBaseByteArray(), notification.getRowKey(), observer.getInterest()
+                    .getColumnFamilyAsHBaseByteArray(), observer.getInterest().getColumnAsHBaseByteArray());
             timer.stop();
         }
     }
@@ -123,14 +120,12 @@ public class ObserverWrapper implements Runnable {
             // Start tx
             tx = tm.begin();
             metrics.observerInvocationEvent(observer.getName());
-            String targetColumnFamily = Constants.HBASE_META_CF;
             // Pattern for notify column in framework's metadata column family: <cf>/<c>-notify
             String notifyColumn = Bytes.toString(columnFamily) + "/" + Bytes.toString(column)
                     + Constants.HBASE_NOTIFY_SUFFIX;
             Get get = new Get(rowKey);
             Result result = txTable.get(tx, get); // Transactional get
-            KeyValue notifyValue = result.getColumnLatest(Bytes.toBytes(targetColumnFamily),
-                    Bytes.toBytes(notifyColumn));
+            KeyValue notifyValue = result.getColumnLatest(Constants.HBASE_META_CF, Bytes.toBytes(notifyColumn));
 
             if (isNotifyFlagSet(notifyValue)) {
                 // Run observer
@@ -153,7 +148,8 @@ public class ObserverWrapper implements Runnable {
             logger.error("Received HBase exception", e);
         } catch (Exception e) {
             metrics.unknownAbortEvent(observer.getName());
-            if (tx != null) tm.rollback(tx);
+            if (tx != null)
+                tm.rollback(tx);
             logger.error("Unhandled exception", e);
         }
     }
@@ -178,16 +174,15 @@ public class ObserverWrapper implements Runnable {
      * @param rowKey
      * @param columnFamily
      * @param column
-     * @throws IOException 
+     * @throws IOException
      */
-    private void clearNotifyFlag(Transaction tx, TTable tt, byte[] rowKey, byte[] columnFamily,
-            byte[] column) throws IOException {
-        String targetColumnFamily = Constants.HBASE_META_CF;
+    private void clearNotifyFlag(Transaction tx, TTable tt, byte[] rowKey, byte[] columnFamily, byte[] column)
+            throws IOException {
         String targetColumn = Bytes.toString(columnFamily) + "/" + Bytes.toString(column)
                 + Constants.HBASE_NOTIFY_SUFFIX;
 
         Put put = new Put(rowKey);
-        put.add(Bytes.toBytes(targetColumnFamily), Bytes.toBytes(targetColumn), Bytes.toBytes("false"));
+        put.add(Constants.HBASE_META_CF, Bytes.toBytes(targetColumn), Bytes.toBytes("false"));
         tt.put(tx, put); // Transactional put
     }
 

src/test/java/com/yahoo/omid/notifications/TestSimpleNotification.java
@@ -115,12 +115,12 @@ public class TestSimpleNotification extends TestInfrastructure {
                     Get row = new Get(rowData.getRow());
                     Result result = tt.getHTable().get(row);
                     byte[] val = result.getValue(
-                            Bytes.toBytes(Constants.HBASE_META_CF),
+                            Constants.HBASE_META_CF,
                             Bytes.toBytes(TestConstants.COLUMN_FAMILY_1 + "/" + TestConstants.COLUMN_1
                                     + Constants.HBASE_NOTIFY_SUFFIX));
                     assertEquals("Values for this row are different", "true", Bytes.toString(val));
                     byte[] rowDataVal = rowData.getValue(
-                            Bytes.toBytes(Constants.HBASE_META_CF),
+                            Constants.HBASE_META_CF,
                             Bytes.toBytes(TestConstants.COLUMN_FAMILY_1 + "/" + TestConstants.COLUMN_1
                                     + Constants.HBASE_NOTIFY_SUFFIX));
                     assertEquals("Values for this row are different", "true", Bytes.toString(rowDataVal));
@@ -178,7 +178,7 @@ public class TestSimpleNotification extends TestInfrastructure {
             byte[] notifyVal = null;
             val = result.getValue(Bytes.toBytes(TestConstants.COLUMN_FAMILY_1), Bytes.toBytes(TestConstants.COLUMN_1));
             notifyVal = result.getValue(
-                    Bytes.toBytes(Constants.HBASE_META_CF),
+                    Constants.HBASE_META_CF,
                     Bytes.toBytes(TestConstants.COLUMN_FAMILY_1 + "/" + TestConstants.COLUMN_1
                             + Constants.HBASE_NOTIFY_SUFFIX));
             assertEquals("Values for this column are different", VAL_1, Bytes.toString(val));

commit dd3c8d9d740923caf70b11c4a9f306bea599ff50

Missing array comparison for HBASE_META_CF fixed. This error was introduced in commit ad4e46480ca99894b40dda7fddd70dab84f3745a

src/main/java/com/yahoo/omid/notifications/TransactionCommittedRegionCoprocessor.java
@@ -20,6 +20,7 @@ import static com.yahoo.omid.notifications.Constants.HBASE_NOTIFY_SUFFIX;
 import static com.yahoo.omid.notifications.Constants.NOTIFY_TRUE;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -67,11 +68,10 @@ public class TransactionCommittedRegionCoprocessor extends BaseRegionObserver {
 
         for (List<KeyValue> kvl : kvs.values()) {
             for (KeyValue kv : kvl) {
-                String cf = Bytes.toString(kv.getFamily());
-
                 // TODO Here we should filter the qualifiers that are not requested to be observed,
                 // but cannot access to the required structures from a BaseRegionObserver
-                if (!cf.equals(HBASE_META_CF)) {
+                if (!Arrays.equals(kv.getFamily(), HBASE_META_CF)) {
+                    String cf = Bytes.toString(kv.getFamily());
                     String q = Bytes.toString(kv.getQualifier());
                     // Pattern for notify qualifier in framework's metadata column family: <cf>/<c>-notify
                     put.add(HBASE_META_CF, Bytes.toBytes(cf + "/" + q + HBASE_NOTIFY_SUFFIX), kv.getTimestamp(),
@@ -101,11 +101,10 @@ public class TransactionCommittedRegionCoprocessor extends BaseRegionObserver {
 
         for (List<KeyValue> kvl : kvs.values()) {
             for (KeyValue kv : kvl) {
-                String cf = Bytes.toString(kv.getFamily());
-
                 // TODO Here we should filter the qualifiers that are not requested to be observed,
                 // but cannot access to the required structures from a BaseRegionObserver
-                if (!cf.equals(HBASE_META_CF)) {
+                if (!Arrays.equals(kv.getFamily(), HBASE_META_CF)) {
+                    String cf = Bytes.toString(kv.getFamily());
                     String q = Bytes.toString(kv.getQualifier());
                     // Pattern for notify qualifier in framework's metadata column family: <cf>/<c>-notify
                     delete.deleteColumn(HBASE_META_CF, Bytes.toBytes(cf + "/" + q + HBASE_NOTIFY_SUFFIX),