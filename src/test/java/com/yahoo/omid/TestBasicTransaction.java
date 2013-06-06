3733e2c6364ceb6aa3fd3a052efdaa3a72bed341
Daniel Gómez Ferro <danielgf@yahoo-inc.com>
2013-06-06 15:02:14 +0200

    Remove wrongly added test

--- src/test/java/com/yahoo/omid/TestBasicTransaction.java
+++ src/test/java/com/yahoo/omid/TestBasicTransaction.java
@@ -45,67 +45,6 @@ import com.yahoo.omid.transaction.TransactionManager;
 public class TestBasicTransaction extends OmidTestBase {
    private static final Log LOG = LogFactory.getLog(TestBasicTransaction.class);
 
-   @Test
-   public void testDifferentConnections() throws Exception {
-       Thread[] threads = new Thread[2];
-
-       Configuration conf = hbaseConf;
-       
-       for (int i = 0; i < threads.length; ++i) {
-           conf = getConf(conf);
-           HTable table = new HTable(conf, TEST_TABLE);
-           threads[i] = new Thread(new Pounder(table, 10000));
-       }
-
-//       assertNotSame(table1.getConnection(), table2.getConnection());
-
-       LOG.info("Threads created");
-       for (int i = 0; i < threads.length; ++i) {
-           threads[i].start();
-       }
-       LOG.info("Threads started");
-       for (int i = 0; i < threads.length; ++i) {
-           threads[i].join();
-       }
-   }
-   
-   Configuration getConf(Configuration conf) {
-       Configuration newConf = new Configuration(conf);
-//       newConf.set(HConstants.ZOOKEEPER_RECOVERABLE_WAITTIME, 
-//               Integer.toString(conf.getInt(HConstants.ZOOKEEPER_RECOVERABLE_WAITTIME, 0) + 1));
-       return newConf;
-   }
-   
-    private class Pounder implements Runnable {
-        private HTable table;
-        private int iterations;
-
-        Pounder(HTable table, int iterations) {
-            this.table = table;
-            this.iterations = iterations;
-        }
-
-        @Override
-        public void run() {
-            StringBuilder sb = new StringBuilder();
-            while (sb.length() < 1000) {
-                sb.append("sdfjslkdfjaksjadkvjnwoienvalsnvlwaknvilsndvlaisjvlawnleivanwoeiafnowijfoaiwjnfoaiwjnafoirej");
-            }
-            byte[] bytes = Bytes.toBytes(sb.toString());
-            Put put = new Put(bytes);
-            put.add(Bytes.toBytes(TEST_FAMILY), Bytes.toBytes("col"), bytes);
-            Get get = new Get(bytes); 
-            for (int i = 0; i < iterations; ++i) {
-                try {
-                    table.put(put);
-                    table.get(get);
-                } catch (IOException e) {
-                    LOG.error("Something wrong", e);
-                    Assert.fail(e.toString());
-                }
-            }
-        }
-    }
     @Test
     public void testTimestampsOfTwoRowsInstertedAfterCommitOfSingleTransactionAreEquals()
             throws Exception {
