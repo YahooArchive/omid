658cdfaf0338a3f538354f4c5e96e2441f64ad32
Matthieu Morel <matthieu@yahoo-inc.com>
2013-06-26 12:30:49 +0200

    Add wall clock time to meters and histograms

--- src/main/java/com/yahoo/omid/notifications/metrics/CsvExtendedReporter.java
+++ src/main/java/com/yahoo/omid/notifications/metrics/CsvExtendedReporter.java
@@ -6,15 +6,20 @@ import java.io.PrintStream;
 import java.util.concurrent.TimeUnit;
 
 import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Histogram;
+import com.yammer.metrics.core.Metered;
 import com.yammer.metrics.core.MetricName;
 import com.yammer.metrics.core.MetricsRegistry;
 import com.yammer.metrics.core.Timer;
 import com.yammer.metrics.reporting.CsvReporter;
 import com.yammer.metrics.stats.Snapshot;
+
 /**
  * A reporter which periodically appends data from each metric to a metric-specific CSV file in an output directory.
- * 
- * For timers it also prints meter information
+ * <p>
+ * For timers it also prints meter information.
+ * <p>
+ * For meters, histograms and timers it also prints the wall clock time taken when generating the output.
  */
 public class CsvExtendedReporter extends CsvReporter {
 
@@ -53,6 +58,32 @@ public class CsvExtendedReporter extends CsvReporter {
 
     public CsvExtendedReporter(MetricsRegistry metricsRegistry, File outputDir) {
         super(metricsRegistry, outputDir);
+    }
+
+    @Override
+    public void processMeter(MetricName name, Metered meter, Context context) throws IOException {
+        final PrintStream stream = context
+                .getStream("# time,count,1 min rate,mean rate,5 min rate,15 min rate,wallclock time");
+        stream.append(
+                new StringBuilder().append(meter.count()).append(',').append(meter.oneMinuteRate()).append(',')
+                        .append(meter.meanRate()).append(',').append(meter.fiveMinuteRate()).append(',')
+                        .append(meter.fifteenMinuteRate()).append(',').append(System.currentTimeMillis()).toString())
+                .println();
+        stream.flush();
+    }
+
+    @Override
+    public void processHistogram(MetricName name, Histogram histogram, Context context) throws IOException {
+        final PrintStream stream = context.getStream("# time,min,max,mean,median,stddev,95%,99%,99.9%,wallclock time");
+        final Snapshot snapshot = histogram.getSnapshot();
+        stream.append(
+                new StringBuilder().append(histogram.min()).append(',').append(histogram.max()).append(',')
+                        .append(histogram.mean()).append(',').append(snapshot.getMedian()).append(',')
+                        .append(histogram.stdDev()).append(',').append(snapshot.get95thPercentile()).append(',')
+                        .append(snapshot.get99thPercentile()).append(',').append(snapshot.get999thPercentile())
+                        .append(',').append(System.currentTimeMillis()).toString()).println();
+        stream.println();
+        stream.flush();
     }
 
     @Override
