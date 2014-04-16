da1bd354b6d6b47b94ae5746dcd9bc63336cc447
Ivan Kelly <ivank@yahoo-inc.com>
2014-04-16 15:06:16 +0200

    Benchmark updated to use new client. Needs metrics update to continue

--- tso/src/main/java/com/yahoo/omid/tso/util/ClientHandler.java
+++ tso/src/main/java/com/yahoo/omid/tso/util/ClientHandler.java
@@ -29,6 +29,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Semaphore;
 
 import org.apache.hadoop.conf.Configuration;
 import org.jboss.netty.channel.Channel;
@@ -42,6 +43,8 @@ import org.slf4j.LoggerFactory;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import com.yahoo.omid.client.TSOClient;
+import com.yahoo.omid.client.TSOFuture;
+
 import com.yahoo.omid.tso.Committed;
 import com.yahoo.omid.tso.RowKey;
 import com.yahoo.omid.tso.TSOMessage;
@@ -54,369 +57,216 @@ import com.yahoo.omid.tso.messages.TimestampResponse;
  * @author maysam
  * 
  */
-public class ClientHandler extends TSOClient {
+public class ClientHandler {
     
     public enum RowDistribution {
         
         UNIFORM,ZIPFIAN
     }
-    
+
+    static int threadCounter = 0;
+    private int threadId = threadCounter++;
+
     private IntegerGenerator intGenerator;
    
-   private CountDownLatch signalInitialized = new CountDownLatch(1);
+    private CountDownLatch signalInitialized = new CountDownLatch(1);
 
-   private static final Logger LOG = LoggerFactory.getLogger(ClientHandler.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ClientHandler.class);
 
 
-   /**
-    * Maximum number of modified rows in each transaction
-    */
-   final int maxTxSize;
-   public static final int DEFAULT_MAX_ROW = 20;
+    /**
+     * Maximum number of modified rows in each transaction
+     */
+    final int maxTxSize;
+    public static final int DEFAULT_MAX_ROW = 20;
 
-   /**
-    * The number of rows in database
-    */
-   private final int dbSize;
-   public static final int DEFAULT_DB_SIZE = 20000000;
+    /**
+     * The number of rows in database
+     */
+    public static final int DEFAULT_DB_SIZE = 20000000;
 
-   public static final long PAUSE_LENGTH = 50; // in ms
+    public static final long PAUSE_LENGTH = 50; // in ms
 
-   /**
-    * Maximum number if outstanding messages
-    */
-   private final int maxInFlight;
+    /**
+     * Maximum number if outstanding messages
+     */
+    private final int maxInFlight;
 
-   /**
-    * Number of message to do
-    */
-   private final long nbMessage;
+    /**
+     * Number of message to do
+     */
+    private final long nbMessage;
 
     /**
      * Number of second to run for
      */
     private volatile boolean timedOut = false;
 
-   /**
-    * Current rank (decreasing, 0 is the end of the game)
-    */
-   private long curMessage;
+    /**
+     * Current rank (decreasing, 0 is the end of the game)
+     */
+    private long curMessage;
 
-   /**
-    * number of outstanding commit requests
-    */
-   private int outstandingTransactions = 0;
+    /**
+     * number of outstanding commit requests
+     */
+    private int outstandingTransactions = 0;
 
-   /**
-    * Start date
-    */
-   private Date startDate = null;
 
-   /**
-    * Stop date
-    */
-   private Date stopDate = null;
+    private long getSizeCom() {
+        return committed.getSize();
+    }
 
-   /**
-    * Return value for the caller
-    */
-   final BlockingQueue<Boolean> answer = new LinkedBlockingQueue<Boolean>();
+    private long getSizeAborted() {
+        return aborted.size() * 8 * 8;
+    }
 
-   private Committed committed = new Committed();
-   private Set<Long> aborted = Collections.synchronizedSet(new HashSet<Long>(100000));
+    private java.util.Random rnd;
+    /**
+     * Start date
+     */
+    private Date startDate = null;
 
-   /*
-    * For statistial purposes
-    */
-   public ConcurrentHashMap<Long, Long> wallClockTime = new ConcurrentHashMap<Long, Long>(); 
+    /**
+     * Stop date
+     */
+    private Date stopDate = null;
 
-   public long totalNanoTime = 0;
-   public long totalTx = 0;
+    /**
+     * Return value for the caller
+     */
+    final BlockingQueue<Boolean> answer = new LinkedBlockingQueue<Boolean>();
+
+    private Committed committed = new Committed();
+    private Set<Long> aborted = Collections.synchronizedSet(new HashSet<Long>(100000));
+
+    /*
+     * For statistial purposes
+     */
+    public ConcurrentHashMap<Long, Long> wallClockTime = new ConcurrentHashMap<Long, Long>(); 
+
+    public long totalNanoTime = 0;
+    public long totalTx = 0;
    
-   private Channel channel;
+    private Channel channel;
 
-   private float percentReads;
+    private float percentReads;
 
+    final Semaphore outstanding;
+    final TSOClient client;
 
+    /**
+     * Constructor
+     * 
+     * @param nbMessage
+     * @param inflight
+     * @throws IOException
+     */
+    public ClientHandler(Configuration conf, int runFor, long nbMessage, int inflight, boolean pauseClient, 
+                         float percentReads, int maxTxSize, IntegerGenerator intGenerator) throws IOException {
+        if (nbMessage < 0) {
+            throw new IllegalArgumentException("nbMessage: " + nbMessage);
+        }
+        this.maxInFlight = inflight;
+        this.nbMessage = nbMessage;
+        this.curMessage = nbMessage;
+        this.pauseClient = pauseClient;
+        this.percentReads = percentReads;
+        this.maxTxSize = maxTxSize;
+        this.intGenerator = intGenerator;
+        this.signalInitialized.countDown();
 
-   /**
-    * Method to wait for the final response
-    * 
-    * @return success or not
-    */
-   public boolean waitForAll() {
-      for (;;) {
-         try {
-            return answer.take();
-         } catch (InterruptedException e) {
-            // Ignore.
-         }
-      }
-   }
+        this.outstanding = new Semaphore(this.maxInFlight);
 
-   /**
-    * Constructor
-    * 
-    * @param nbMessage
-    * @param inflight
-    * @throws IOException
-    */
-   public ClientHandler(Configuration conf, int runFor, long nbMessage, int inflight, boolean pauseClient, 
-           float percentReads, int maxTxSize, final int dbSize, IntegerGenerator intGenerator) throws IOException {
-       super(conf);
-       if (nbMessage < 0) {
-           throw new IllegalArgumentException("nbMessage: " + nbMessage);
-       }
-       this.maxInFlight = inflight;
-       this.nbMessage = nbMessage;
-       this.curMessage = nbMessage;
-       this.pauseClient = pauseClient;
-       this.percentReads = percentReads;
-       this.maxTxSize = maxTxSize;
-       this.dbSize = dbSize;
-       this.intGenerator = intGenerator;
-       this.signalInitialized.countDown();
+        client = new TSOClient(conf);
 
-       executor.schedule(new Runnable() {
-               @Override
-               public void run() {
-                   timedOut = true;
-               }
-           }, runFor, TimeUnit.SECONDS);
-   }
+        long seed = System.currentTimeMillis();
+        seed *= channel.getId();// to make it channel dependent
+        rnd = new java.util.Random(seed);
 
-   /**
-    * Starts the traffic
-    */
-    //   @Override FIXME
-   public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) {
-       //super.channelConnected(ctx, e);
-      try {
-          // don't serve before ClientHandler fully initialized
-          signalInitialized.await();
-      } catch (InterruptedException e1) {
-          throw new RuntimeException(e1);
-      }
-      startDate = new Date();
-      channel = e.getChannel();
-      outstandingTransactions = 0;
-      startTransaction();
-   }
+        executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            outstanding.acquire();
+                            final TSOFuture<Long> f = client.createTransaction();
+                            f.addListener(new TimestampListener(f), executor);
+                        }
+                    } catch (InterruptedException ie) {
+                        // normal, ignore, we're shutting down
+                    }
+                }
+            });
+    }
 
-   
-    //@Override
-   synchronized public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
-       // TSO client will try to reconnect. Ignore that behaviour for this test
-   } 
+    class TimestampListener implements Runnable {
+        TSOFuture<Long> f;
 
-/**
-    * If write of Commit Request was not possible before, just do it now
-    */
-    //@Override
-   public void channelInterestChanged(ChannelHandlerContext ctx, ChannelStateEvent e) {
-      startTransaction();
-   }
+        TimestampListener(TSOFuture<Long> f) {
+            
+        }
 
-   /**
-    * When the channel is closed, print result
-    */
-    //@Override
-   public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
-       //super.channelClosed(ctx, e);
-      stopDate = new Date();
-      String MB = String.format("Memory Used: %8.3f MB", (Runtime.getRuntime().totalMemory() - Runtime.getRuntime()
-            .freeMemory()) / 1048576.0);
-      String Mbs = String.format("%9.3f TPS",
-            ((nbMessage - curMessage) * 1000 / (float) (stopDate.getTime() - (startDate != null ? startDate.getTime()
-                  : 0))));
-      LOG.info(MB + " " + Mbs);
-   }
+        @Override
+        public void run() {
+            ;
+        }
+    }
 
-   /**
-    * When a message is received, handle it based on its type
-    * @throws IOException 
-    */
-   //@Override
-   protected void processMessage(TSOMessage msg) {
-      if (msg instanceof CommitResponse) {
-         handle((CommitResponse) msg);
-      } else if (msg instanceof TimestampResponse) {
-         handle((TimestampResponse) msg);
-      }
-   }
+    class DeferredListener implements Runnable {
+        final long txnid;
 
-   /**
-    * Handle the TimestampResponse message
-    */
-   public void handle(TimestampResponse timestampResponse) {
-      sendCommitRequest(timestampResponse.timestamp);
-   }
+        DeferredListener(long txnid) {
+            this.txnid = txnid;
+        }
 
-   /**
-    * Handle the CommitRequest message
-    */
-   private long lasttotalTx = 0;
-   private long lasttotalNanoTime = 0;
-   private long lastTimeout = System.currentTimeMillis();
+        @Override
+        public void run() {
+            boolean readOnly = (rnd.nextFloat() * 100) < percentReads;
 
-   public void handle(CommitResponse msg) {
-      // outstandingTransactions.decrementAndGet();
-      outstandingTransactions--;
-      long finishNanoTime = System.nanoTime();
-      long startNanoTime = wallClockTime.remove(msg.startTimestamp);
-      if (msg.committed) {
-         totalNanoTime += (finishNanoTime - startNanoTime);
-         totalTx++;
-         long timeout = System.currentTimeMillis();
-         // if (totalTx % 10000 == 0) {//print out
-         if (timeout - lastTimeout > 60 * 1000) { // print out
-            long difftx = totalTx - lasttotalTx;
-            long difftime = totalNanoTime - lasttotalNanoTime;
-            System.out.format(
-                  " CLIENT: totalTx: %d totalNanoTime: %d microtime/tx: %4.3f tx/s %4.3f "
-                        + "Size Com: %d Size Aborted: %d Memory Used: %8.3f KB TPS:  %9.3f \n",
-                  difftx,
-                  difftime,
-                  (difftime / (double) difftx / 1000),
-                  1000 * difftx / ((double) (timeout - lastTimeout)),
-                  getSizeCom(),
-                  getSizeAborted(),
-//                  largestDeletedTimestamp - _decoder.lastStartTimestamp,
-                  (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024.0,
-                  ((nbMessage - curMessage) * 1000 / (float) (new Date().getTime() - (startDate != null ? startDate
-                        .getTime() : 0))));
-            lasttotalTx = totalTx;
-            lasttotalNanoTime = totalNanoTime;
-            lastTimeout = timeout;
-         }
-      } else {// aborted
-         // try {
-         //    super.completeAbort(msg.startTimestamp, new SyncAbortCompleteCallback());
-         // } catch (IOException e) {
-         //    LOG.error("Couldn't send abort", e);
-         // }
-      }
-      startTransaction();
-   }
-
-   private long getSizeCom() {
-      return committed.getSize();
-   }
-
-   private long getSizeAborted() {
-      return aborted.size() * 8 * 8;
-   }
-
-   private java.util.Random rnd;
-
-   private boolean pauseClient;
-
-   /**
-    * Sends the CommitRequest message to the channel
-    * 
-    * @param timestamp
-    * @param channel
-    */
-   private void sendCommitRequest(final long timestamp) {
-      if (!((channel.getInterestOps() & Channel.OP_WRITE) == 0))
-         return;
-
-      // initialize rnd if it is not yet
-      if (rnd == null) {
-         long seed = System.currentTimeMillis();
-         seed *= channel.getId();// to make it channel dependent
-         rnd = new java.util.Random(seed);
-      }
-
-      boolean readOnly = (rnd.nextFloat() * 100) < percentReads;
-
-      int size = readOnly ? 0 : rnd.nextInt(maxTxSize);
-      final RowKey [] rows = new RowKey[size];
-      for (byte i = 0; i < rows.length; i++) {
-         long l = intGenerator.nextInt();
-         byte[] b = new byte[8];
-         for (int iii = 0; iii < 8; iii++) {
-            b[7 - iii] = (byte) (l >>> (iii * 8));
-         }
-         byte[] tableId = new byte[8];
-         rows[i] = new RowKey(b, tableId);
-      }
-
-      // send a query once in a while
-      totalCommitRequestSent++;
-      if (totalCommitRequestSent % QUERY_RATE == 0 && rows.length > 0) {
-         long queryTimeStamp = rnd.nextInt(Math.abs((int) timestamp));
-         try {
-             //isCommitted(timestamp, queryTimeStamp, new SyncCommitQueryCallback());
-             throw new IOException();
-         } catch (IOException e) {
-            LOG.error("Couldn't send commit query", e);
-         }
-      }
-
-      executor.schedule(new Runnable() {
-         @Override
-         public void run() {
-            // keep statistics
-            wallClockTime.put(timestamp, System.nanoTime());
-
-            // try {
-            //    commit(timestamp, rows, new SyncCommitCallback());
-            // } catch (IOException e) {
-            //    LOG.error("Couldn't send commit", e);
-            //    e.printStackTrace();
-            // }
-         }
-      }, pauseClient ? PAUSE_LENGTH : 0, TimeUnit.MILLISECONDS);
-
-   }
-
-	private static ScheduledExecutorService executor = Executors.newScheduledThreadPool(20, new ThreadFactoryBuilder()
-			.setNameFormat("Commit handler %d").build());
-
-   private long totalCommitRequestSent;// just to keep the total number of
-                                       // commitreqeusts sent
-   private int QUERY_RATE = 100;// send a query after this number of commit
-                                // requests
-
-   /**
-    * Start a new transaction
-    * 
-    * @param channel
-    * @throws IOException 
-    */
-   private void startTransaction() {
-      while (true) {// fill the pipe with as much as request you can
-         if (!((channel.getInterestOps() & Channel.OP_WRITE) == 0))
-            return;
-
-         // if (outstandingTransactions.intValue() >= MAX_IN_FLIGHT)
-         if (outstandingTransactions >= maxInFlight)
-            return;
-
-         if (curMessage == 0 || timedOut) {
-            // wait for all outstanding msgs and then close the channel
-            // if (outstandingTransactions.intValue() == 0) {
-            if (outstandingTransactions == 0) {
-                LOG.info("No more messages and all outstanding messages consumed. Stopping benchmark, closing channel ["
-                        + channel.getId() + "]");
-                channel.close().addListener(new ChannelFutureListener() {
-                  public void operationComplete(ChannelFuture future) {
-                     answer.offer(true);
-                  }
-               });
+            int size = readOnly ? 0 : rnd.nextInt(maxTxSize);
+            final RowKey [] rows = new RowKey[size];
+            for (byte i = 0; i < rows.length; i++) {
+                long l = intGenerator.nextInt();
+                byte[] b = new byte[8];
+                for (int iii = 0; iii < 8; iii++) {
+                    b[7 - iii] = (byte) (l >>> (iii * 8));
+                }
+                byte[] tableId = new byte[8];
+                rows[i] = new RowKey(b, tableId);
             }
-            return;
-         }
-         curMessage--;
-         outstandingTransactions++;
-         // try {
-         //    super.getNewTimestamp(new SyncCreateCallback());
-         // } catch (IOException e) {
-         //    LOG.error("Couldn't start transaction", e);
-         // }
+            final TSOFuture<Long> f = client.commit(txnid, rows);
+            f.addListener(new CommitListener(f), executor);
+        }
+    }
 
-         Thread.yield();
-      }
-   }
+    class CommitListener implements Runnable {
+        TSOFuture<Long> f;
+
+        CommitListener(TSOFuture<Long> f) {
+            this.f = f;
+        }
+
+        @Override
+        public void run() {
+            outstanding.release();
+        }
+    }
+            
+    void shutdown() {
+    }
+
+
+
+    private boolean pauseClient;
+
+    private ScheduledExecutorService executor = Executors.newScheduledThreadPool(2,
+            new ThreadFactoryBuilder().setNameFormat("client-thread-" + threadId + "-%d").build());
+
+    private long totalCommitRequestSent;// just to keep the total number of
+    // commitreqeusts sent
+    private int QUERY_RATE = 100;// send a query after this number of commit
+    // requests
+
+
 }
--- tso/src/main/java/com/yahoo/omid/tso/util/TransactionClient.java
+++ tso/src/main/java/com/yahoo/omid/tso/util/TransactionClient.java
@@ -50,7 +50,6 @@ public class TransactionClient {
         
         System.out.println("Starting " + config.nbClients + " clients with the following configuration:");
         System.out.println("PARAM MAX_ROW: " + config.maxTxSize);
-        System.out.println("PARAM DB_SIZE: " + config.dbSize);
         System.out.println("pause " + config.pause);
         
         float readPercentage = config.readproportion==-1?config.percentReads:(config.readproportion * 100);
@@ -61,41 +60,43 @@ public class TransactionClient {
         IntegerGenerator[] intGenerators = new IntegerGenerator[config.nbClients];
         
         System.out.println("Initializing row ids generators for distribution ["+config.requestDistribution + "] (that may take a while)");
+
         // zipfian generator takes a while to initialize. Do that first
         for(int i = 0; i < intGenerators.length; ++i) {
             if (rowDistribution.equals(RowDistribution.UNIFORM)) {
                 intGenerators[i] = new IntegerGenerator() {
-                    Random r = new Random();
-                    @Override
-                    public int nextInt() {
-                        return r.nextInt(config.dbSize);
-                    }
+                        Random r = new Random();
+                        @Override
+                        public int nextInt() {
+                            return r.nextInt(Integer.MAX_VALUE);
+                        }
                     
-                    @Override
-                    public double mean() {
-                        // TODO Auto-generated method stub
-                        return 0;
-                    }
-                };
+                        @Override
+                        public double mean() {
+                            // TODO Auto-generated method stub
+                            return 0;
+                        }
+                    };
             } else {
-                intGenerators[i] = new ScrambledZipfianGenerator(config.dbSize);
+                intGenerators[i] = new ScrambledZipfianGenerator(Long.MAX_VALUE);
             }
         }
 
         for(int i = 0; i < config.nbClients; ++i) {
-         // Create the associated Handler
+            // Create the associated Handler
             ClientHandler handler = new ClientHandler(conf, config.runFor, config.nbMessages,
                                                       config.maxInFlight, config.pause,
                                                       readPercentage, config.maxTxSize,
-                                                      config.dbSize, intGenerators[i]);
-   
-           // *** Start the Netty running ***
-           handlers.add(handler);
+                                                      intGenerators[i]);
+            // *** Start the Netty running ***
+            handlers.add(handler);
         }
+
         
-        // Wait for the Traffic to finish
+        Thread.sleep(config.runFor * 1000);
+
         for (ClientHandler handler : handlers) {
-           handler.waitForAll();
+            handler.shutdown();
         }
         
         System.out.println("\n**********\nBenchmark complete - please check the metrics from individual client threads in the console / log");
@@ -153,9 +154,6 @@ public class TransactionClient {
         
         @Parameter(names="-maxTxSize", description="Maximum size of transaction (size homogeneously distributed between 1 and this number)")
         int maxTxSize = ClientHandler.DEFAULT_MAX_ROW;
-        
-        @Parameter(names="-dbSize", description="Total number of rows in the database")
-        int dbSize = ClientHandler.DEFAULT_DB_SIZE;
         
         @Parameter(names="-requestDistribution", description="Request distribution (how to pick rows) {uniform|zipfian}"  )
         String requestDistribution = "uniform";
