207b5b33740c2a831c47961cef2e6278c37ec00b
Ivan Kelly <ivank@yahoo-inc.com>
2014-10-02 16:45:41 +0200

    Bugzilla Ticket 7118713 - IOException: A value is already present for key ...<> This should not happen
    The root cause was that there are situations on a failed commit that
    multiple deletes are generated. When a minor compaction happened on a
    hfile with these deletes, the compactor puked. This patch fixes that.
    
    Change-Id: Ia483f94785ffab816625b597c5071e65c26e7f0e

--- tso-server/src/main/java/com/yahoo/omid/metrics/NullMetricsProvider.java
+++ tso-server/src/main/java/com/yahoo/omid/metrics/NullMetricsProvider.java
@@ -25,37 +25,29 @@ public class NullMetricsProvider implements MetricsProvider, MetricsRegistry {
 
     @Override
     public <T extends Number> void gauge(String name, Gauge<T> gauge) {
-        LOG.trace("Null gauge (name={}) registered", name);
     }
 
     @Override
     public Counter counter(final String name) {
-
-        LOG.trace("Null counter (name={}) registered", name);
-
         return new Counter() {
 
             @Override
             public void inc() {
-                LOG.trace("Null counter (name={}) incremented", name);
                 // Do nothing
             }
 
             @Override
             public void inc(long n) {
-                LOG.trace("Null counter (name={}) incremented by {}", name, n);
                 // Do nothing
             }
 
             @Override
             public void dec() {
-                LOG.trace("Null counter (name={}) decremented", name);
                 // Do nothing
             }
 
             @Override
             public void dec(long n) {
-                LOG.trace("Null counter (name={}) decremented by {}", name, n);
                 // Do nothing
             }
 
@@ -66,24 +58,19 @@ public class NullMetricsProvider implements MetricsProvider, MetricsRegistry {
     @Override
     public Timer timer(final String name) {
 
-        LOG.trace("Null timer (name={}) registered", name);
-
         return new Timer() {
             @Override
             public void start() {
-                LOG.trace("Null timer (name={}) started", name);
                 // Do nothing
             }
 
             @Override
             public void stop() {
-                LOG.trace("Null timer (name={}) stopped", name);
                 // Do nothing
             }
 
             @Override
             public void update(long duration) {
-                LOG.trace("Null timer (name={}) updated with duration {}", name, duration);
                 // Do nothing
             }
         };
@@ -93,19 +80,15 @@ public class NullMetricsProvider implements MetricsProvider, MetricsRegistry {
     @Override
     public Meter meter(final String name) {
 
-        LOG.trace("Null meter (name={}) registered", name);
-
         return new Meter() {
 
             @Override
             public void mark() {
-                LOG.trace("Null meter (name={}) marked", name);
                 // Do nothing
             }
 
             @Override
             public void mark(long n) {
-                LOG.trace("Null meter (name={}) marked by {}", name, n);
                 // Do nothing
             }
 
@@ -115,19 +98,15 @@ public class NullMetricsProvider implements MetricsProvider, MetricsRegistry {
     @Override
     public Histogram histogram(final String name) {
 
-        LOG.trace("Null histogram (name={}) registered", name);
-
         return new Histogram() {
 
             @Override
             public void update(long value) {
-                LOG.trace("Null histogram (name={}) updated by {}", name, value);
                 // Do nothing
             }
 
             @Override
             public void update(int value) {
-                LOG.trace("Null histogram (name={}) updated by {}", name, value);
                 // Do nothing
             }
         };
