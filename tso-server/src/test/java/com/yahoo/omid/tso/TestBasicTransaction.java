d367b1e21dcfa1de88b62989efe477a7088fd261
Francisco Perez-Sorrosal <fperez@yahoo-inc.com>
2015-02-04 14:34:05 +0100

    Increase test timeouts in TSO server module (II)
    Complement to commit 9d25a29760cdd866381ac6342831a5eeffd879b4
    
    Also identified duplicated test code
    
    Change-Id: I856fccd6f98c5828fe992b40168140935690fa62

--- tso-server/src/test/java/com/yahoo/omid/tso/TestBasicTransaction.java
+++ tso-server/src/test/java/com/yahoo/omid/tso/TestBasicTransaction.java
@@ -1,19 +1,3 @@
-/**
- * Copyright (c) 2011 Yahoo! Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. See accompanying LICENSE file.
- */
-
 package com.yahoo.omid.tso;
 
 import static org.testng.AssertJUnit.assertEquals;
@@ -24,9 +8,11 @@ import java.util.concurrent.ExecutionException;
 import com.google.common.collect.Sets;
 import com.yahoo.omid.tsoclient.TSOClient.AbortException;
 
+// TODO: This test is effectively the same as TestConflict. Clean these tests in a
+// future commit, removing duplicated code
 public class TestBasicTransaction extends TSOTestBase {
 
-    @Test(timeOut=10000)
+    @Test(timeOut = 30000)
     public void testConflicts() throws Exception {
         long tr1 = client.getNewStartTimestamp().get();
         long tr2 = client.getNewStartTimestamp().get();
