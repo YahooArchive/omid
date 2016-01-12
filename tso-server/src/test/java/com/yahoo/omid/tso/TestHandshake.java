366fa2005f4228547474e6bbd050005afef63ad4
Francisco Perez-Sorrosal <fperez@yahoo-inc.com>
2016-01-12 15:28:21 -0800

    Avoid unrequired test infrastructure in TestHandshake
    There was no need of standard TSO client, what improves test duration in around 10 secs
    
    Change-Id: I678e83bc154426647f87a72815fcf29b2c467143

--- tso-server/src/test/java/com/yahoo/omid/tso/TestHandshake.java
+++ tso-server/src/test/java/com/yahoo/omid/tso/TestHandshake.java
@@ -1,24 +1,56 @@
 package com.yahoo.omid.tso;
 
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.fail;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.yahoo.omid.TestUtils;
+import com.yahoo.omid.proto.TSOProto;
+import com.yahoo.omid.tsoclient.TSOClient.ConnectionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.ExecutionException;
 
-import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
 
-import com.yahoo.omid.proto.TSOProto;
-import com.yahoo.omid.tsoclient.TSOClient;
+public class TestHandshake  {
 
-public class TestHandshake extends TSOTestBase {
+    private static final Logger LOG = LoggerFactory.getLogger(TestHandshake.class);
+
+    private final int TSO_SERVER_PORT = 1234;
+
+    // Component under test
+    private TSOServer tso;
+
+    @BeforeClass
+    public void setup() throws Exception {
+
+        Module tsoServerMockModule = new TSOMockModule(TSOServerCommandLineConfig.configFactory(TSO_SERVER_PORT, 1000));
+        Injector injector = Guice.createInjector(tsoServerMockModule);
+
+        LOG.info("==================================================================================================");
+        LOG.info("======================================= Init TSO Server ==========================================");
+        LOG.info("==================================================================================================");
+
+        tso = injector.getInstance(TSOServer.class);
+        tso.startAndWait();
+        TestUtils.waitForSocketListening("localhost", TSO_SERVER_PORT, 100);
+
+        LOG.info("==================================================================================================");
+        LOG.info("===================================== TSO Server Initialized =====================================");
+        LOG.info("==================================================================================================");
+    }
+
     /**
-     * Test that if a client tries to make a request
-     * without handshaking, it will be disconnected.
+     * Test that if a client tries to make a request without handshaking, it will be disconnected.
      */
-    @Test(timeOut=10000)
+    @Test(timeOut = 10_000)
     public void testOldClientCurrentServer() throws Exception {
-        TSOClientRaw raw = new TSOClientRaw(clientConf.getString("tso.host"),
-                                            clientConf.getInt("tso.port"));
+
+        TSOClientRaw raw = new TSOClientRaw("localhost", TSO_SERVER_PORT);
 
         TSOProto.Request request = TSOProto.Request.newBuilder()
             .setTimestampRequest(TSOProto.TimestampRequest.newBuilder().build())
@@ -26,11 +58,12 @@ public class TestHandshake extends TSOTestBase {
         raw.write(request);
         try {
             raw.getResponse().get();
-            fail("Channel should close");
+            fail("Channel should be closed");
         } catch (ExecutionException ee) {
-            assertEquals("Should be channel closed exception",
-                    TSOClient.ConnectionException.class, ee.getCause().getClass());
+            assertEquals(ee.getCause().getClass(), ConnectionException.class, "Should be channel closed exception");
         }
         raw.close();
+
    }
+
 }
